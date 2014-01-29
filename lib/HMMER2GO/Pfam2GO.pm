6d659de0cadefeb180128dafd82bf5c5064ef11d
sestaton <statonse@gmail.com>
2014-01-29 06:36:46 -0500

    add code to get mapping file

---
+++ lib/HMMER2GO/Pfam2GO.pm
@@ -0,0 +1,31 @@
+package Pfam2GO;
+
+use Moo;
+use LWP::UserAgent;
+
+
+## fetch pfam2go file http://www.geneontology.org/external2go/pfam2go
+my $pfam2go = 'pfam2go';
+
+my $ua = LWP::UserAgent->new;
+
+my $urlbase = 'http://www.geneontology.org/external2go/pfam2go ';
+my $response = $ua->get($urlbase);
+
+#
+# check for a response
+#
+unless ($response->is_success) {
+    die "Can't get url $urlbase -- ", $response->status_line;
+}
+
+#
+# cpen and parse the results
+#
+open my $out, '>', $pfam2go or die "\nERROR: Could not open file: $!\n";
+say $out $response->content;
+close $out;
+
+## then map terms
+
+1;
