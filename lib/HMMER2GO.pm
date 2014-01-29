4702e7e0a5586b33e2237eccf78dd537307908f8
sestaton <statonse@gmail.com>
2014-01-29 06:36:34 -0500

    initial commit of base class

---
+++ lib/HMMER2GO.pm
@@ -0,0 +1,87 @@
+package HMMER2GO;
+
+use 5.012;
+use Moo;
+use MooX::Options
+use namespace::autoclean;
+
+
+=head1 NAME
+
+HMMER2GO - Map GO terms to domain search results
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    hmmer2go --map ...
+
+=cut
+
+option 'infile' => (
+      is => 'ro',
+      format => 's',
+      required => 1,
+      doc => 'the file to display'
+  );
+
+option 'pfam2go' => ( );
+
+option 'map' => ( );
+
+=head1 AUTHOR
+
+S. Evan Staton, C<< <statonse at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the project site at 
+L<https://github.com/sestaton/HMMER2GO/issues>. I will be notified,
+and there will be a record of the issue. Alternatively, I can also be 
+reached at the email address listed above to resolve any questions.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc HMMER2GO
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 S. Evan Staton.
+
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+
+=cut
+
+1;
