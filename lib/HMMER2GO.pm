ff83d52fcb853cbf85d1fcd8b657f69344a44ff9
sestaton <statonse@gmail.com>
2014-06-09 13:54:47 -0400

    add pod to main class

--- lib/HMMER2GO.pm
+++ lib/HMMER2GO.pm
@@ -2,6 +2,43 @@ package HMMER2GO;
  
 use App::Cmd::Setup -app;
 
+=head1 NAME
+
+HMMER2GO - Annotate DNA sequences for Gene Ontology terms
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
 our $VERSION = '0.01';
+
+=head1 AUTHOR
+
+S. Evan Staton, C<< <statonse at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the project site at 
+L<https://github.com/sestaton/HMMER2GO/issues>. I will be notified,
+and there will be a record of the issue. Alternatively, I can also be 
+reached at the email address listed above to resolve any questions.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc HMMER2GO
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 S. Evan Staton
+
+This program is distributed under the MIT (X11) License, which should be distributed with the package. 
+If not, it can be found here: L<http://www.opensource.org/licenses/mit-license.php>
+
+=cut
 
 1;
