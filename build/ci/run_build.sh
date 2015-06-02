40d5ce3fedfe5dc8ce9a6759b828e0aa301b7145
sestaton <statonse@gmail.com>
2015-06-02 16:10:05 -0700

    fix unarchive command

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
-hmmer-3.1b2-linux-intel-x86_64.tar.gz
+tar xzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
 cp hmmer-3.1b2-linux-intel-x86_64/binaries/* /usr/bin
