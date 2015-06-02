e19d679eed185bbd0a0e8d5f429041501edd3537
sestaton <statonse@gmail.com>
2015-06-02 16:21:50 -0700

    give correct permissions for install

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -2,4 +2,4 @@
 
 wget http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
 tar xzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
-cp hmmer-3.1b2-linux-intel-x86_64/binaries/* /usr/bin
+sudo cp hmmer-3.1b2-linux-intel-x86_64/binaries/* /usr/bin
