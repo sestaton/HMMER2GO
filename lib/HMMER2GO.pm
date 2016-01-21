a33eb69455a62766db885f002ac54161aea2e720
sestaton <statonse@gmail.com>
2016-01-21 12:38:41 -0800

    bump version

--- lib/HMMER2GO.pm
+++ lib/HMMER2GO.pm
@@ -8,11 +8,11 @@ HMMER2GO - Annotate DNA sequences for Gene Ontology terms
 
 =head1 VERSION
 
-Version 0.17
+Version 0.17.1
 
 =cut
 
-our $VERSION = '0.17';
+our $VERSION = '0.17.1';
 
 sub global_opt_spec {
     [ 'man|m' => "Get the manual entry for a command" ];
