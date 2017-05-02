e6f76fd6b0608d0eab50a3e97560608fc47d7f28
sestaton <statonse@gmail.com>
2017-05-02 11:28:02 -0700

    bump version

--- lib/HMMER2GO/Command.pm
+++ lib/HMMER2GO/Command.pm
@@ -8,11 +8,11 @@ HMMER2GO::Command - Control class for setting global options with HMMER2GO
 
 =head1 VERSION
 
-Version 0.17.3
+Version 0.17.4
 
 =cut
 
-our $VERSION = '0.17.3';
+our $VERSION = '0.17.4';
 
 sub opt_spec {
     my ( $class, $app ) = @_;
