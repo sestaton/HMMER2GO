25d527cb077d4cb89320a2cbcf3320cbcfbf7836
sestaton <1477856+sestaton@users.noreply.github.com>
2018-04-19 15:28:59 -0700

    see if the network issue is with coveralls

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Testing connection GO website"
-wget ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
+#echo "Testing connection to GO website"
+#wget ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
 
 wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
 tar xzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
