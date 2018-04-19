af737a2b7bda1635cbf02fbe612efb3f80db55ee
sestaton <1477856+sestaton@users.noreply.github.com>
2018-04-19 15:22:06 -0700

    add debug for network test

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Testing connection GO website"
+wget ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
+
 wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
 tar xzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
 sudo cp hmmer-3.1b2-linux-intel-x86_64/binaries/* /usr/bin
