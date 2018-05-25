c41a5e2540f423a76aad8d4226cf1966405617f6
sestaton <1477856+sestaton@users.noreply.github.com>
2018-05-25 13:23:00 -0700

    fetch files before install to get around network issue with travis builds

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -6,3 +6,8 @@
 wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz
 tar xzf hmmer-3.1b2-linux-intel-x86_64.tar.gz
 sudo cp hmmer-3.1b2-linux-intel-x86_64/binaries/* /usr/bin
+
+cd t/test_data
+echo "Fetching GO mapping files"
+wget ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
+wget ftp://ftp.geneontology.org/pub/go/doc/GO.terms_alt_ids
