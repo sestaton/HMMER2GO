f8fc1e269da6cafb55597b8168ac9ca238da3bce
sestaton <1477856+sestaton@users.noreply.github.com>
2018-05-25 13:42:24 -0700

    try curl with travis vars for fetching data

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -9,5 +9,16 @@ sudo cp hmmer-3.1b2-linux-intel-x86_64/binaries/* /usr/bin
 
 cd t/test_data
 echo "Fetching GO mapping files"
-wget ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
-wget ftp://ftp.geneontology.org/pub/go/doc/GO.terms_alt_ids
+#wget ftp://ftp.geneontology.org/pub/go/external2go/pfam2go
+#wget ftp://ftp.geneontology.org/pub/go/doc/GO.terms_alt_ids
+
+curl --verbose --progress-bar --ipv4 --connect-timeout 8 \
+     --max-time 120 --retry 128 --ftp-ssl --disable-epsv --ftp-pasv \
+     -u "travis:${TRAVIS_FTP_PASS}" ftp://ftp.geneontology.org/pub/go/external2go/pfam2go \
+     --output pfam2go
+
+curl --verbose --progress-bar --ipv4 --connect-timeout 8 \
+     --max-time 120 --retry 128 --ftp-ssl --disable-epsv --ftp-pasv \
+     -u "travis:${TRAVIS_FTP_PASS}" ftp://ftp.geneontology.org/pub/go/doc/GO.terms_alt_ids \
+     --output GO.terms_alt_ids
+
