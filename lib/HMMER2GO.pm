e3757f940c823421476dd559b7b4389d478f7b9b
sestaton <1477856+sestaton@users.noreply.github.com>
2018-05-25 14:18:21 -0700

    update version

--- lib/HMMER2GO.pm
+++ lib/HMMER2GO.pm
@@ -8,11 +8,11 @@ HMMER2GO - Annotate DNA sequences for Gene Ontology terms
 
 =head1 VERSION
 
-Version 0.18.0
+Version 0.17.8
 
 =cut
 
-our $VERSION = '0.18.0';
+our $VERSION = '0.17.8';
 
 sub global_opt_spec {
     [ 'man|m' => "Get the manual entry for a command" ];
--- lib/HMMER2GO/Command.pm
+++ lib/HMMER2GO/Command.pm
@@ -8,11 +8,11 @@ HMMER2GO::Command - Control class for setting global options with HMMER2GO
 
 =head1 VERSION
 
-Version 0.18.0
+Version 0.17.8
 
 =cut
 
-our $VERSION = '0.18.0';
+our $VERSION = '0.17.8';
 
 sub opt_spec {
     my ( $class, $app ) = @_;
